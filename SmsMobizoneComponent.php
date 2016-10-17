dafef3b61a6f4e5bdfde5e06056552048a07da2a
xalberteinsteinx <xalbert.einsteinx@gmail.com>
2016-10-17 17:18:33 +0300

    Some properties and methods added.

--- SmsMobizoneComponent.php
+++ SmsMobizoneComponent.php
@@ -2,6 +2,11 @@
 namespace bl\cms\sms;
 
 use yii\base\Component;
+use yii\base\Exception;
+use yii\helpers\ArrayHelper;
+use yii\httpclient\Client;
+use yii\web\BadRequestHttpException;
+
 
 /**
  * This is the component class for Mobizone API
@@ -11,47 +16,122 @@ use yii\base\Component;
  */
 class SmsMobizoneComponent extends Component
 {
-    private $apiToken;
-    private $apiServer = 'https://api.mobizon.com/';
-    private $format = 'JSON';
+    /**
+     * @var string $apiToken
+     * For get API token you must register account at https://mobizon.net.ua
+     * After this turn on API access on "API settings" panel section (https://mobizon.net.ua/panel) and get your token.
+     */
+    public $apiToken;
 
-    public $phoneNumber;
+    /**
+     * @var string $recipientPhoneNumber
+     * The recipient of an SMS message.
+     * This phone number must be in international format without plus.
+     * Example: "380965550000".
+     */
+    public $recipientPhoneNumber;
+
+    /**
+     * @var string $defaultMessage
+     * Text message.
+     */
     public $defaultMessage;
 
-    public function __construct($config)
+    /**
+     * @var string $alphaName
+     * This is your signature in SMS.
+     * For use this property you must create alpha-name in your account (https://mobizon.net.ua/panel)
+     * on "My signatures" panel section.
+     * This property can be empty.
+     */
+    public $alphaName;
+
+
+    private $apiServer = 'https://api.mobizon.com/service/';
+    private $apiVersion = 'v1';
+    private $httpMethod = 'POST';
+    private $format = 'json';
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct()
     {
-        $this->apiToken = $config['apiToken'];
-        $this->apiServer = $config['apiServer'];
-        $this->format = $config['format'];
-
-        $this->phoneNumber = $config['phoneNumber'];
-        $this->defaultMessage = $config['defaultMessage'];
-
         parent::__construct();
     }
 
-    protected function callMethod($model, $method) {
-        $data = [
-            'apiKey' => $this->apiToken,
-        ];
+    /**
+     * This methid is using for calling to Mobizone API
+     *
+     * @param string $model
+     * @param string $method
+     * @param array $params
+     *
+     * @return array
+     * @throws BadRequestHttpException
+     */
+    private function callMethod($model, $method, $params = [])
+    {
+        $data = ArrayHelper::merge([
+            'apiKey' => $this->apiToken, 'output' => $this->format, 'api' => $this->apiVersion],
+            $params
+        );
 
-        $url = $this->apiServer . $model . '/' . $method;
-
-        $post = json_encode($data);
-
-        $result = file_get_contents($url, null, stream_context_create([
-            'http' => [
-                'method' => 'POST',
-                'header' => "Content-type: application/x-www-form-urlencoded;\r\n",
-                'content' => $post,
-            ]
-        ]));
-
-        return $result;
+        $client = new Client();
+        $response = $client->createRequest()
+            ->setMethod($this->httpMethod)
+            ->setUrl($this->apiServer . $model . '/' . $method)
+            ->setData($data)
+            ->send();
+        if ($response->isOk) {
+            return $response->data;
+        }
+        else throw new BadRequestHttpException();
     }
 
+    /**
+     * This method returns user account balance
+     *
+     * @return array
+     */
     public function getBalance() {
-        $balance = $this->callMethod('user', 'getownbalance');
-        return $balance;
+        $balance = $this->callMethod('User', 'GetOwnBalance');
+        return $balance['data']['balance'];
+    }
+
+    /**
+     * This method sends one SMS.
+     * Returns message id.
+     *
+     * @param string $recipient
+     * @param string $text
+     * @param string $alphaName
+     *
+     * @return integer
+     * @throws BadRequestHttpException
+     * @throws Exception
+     */
+    public function send($recipient = null, $text = null, $alphaName = null) {
+
+        $recipient = (!empty($recipient)) ? $recipient : $this->recipientPhoneNumber;
+        $text = (!empty($text)) ? $text : $this->defaultMessage;
+        $alphaName = (!empty($alphaName)) ? $alphaName : $this->alphaName;
+
+        if (empty($recipient)) {
+            throw new BadRequestHttpException('Recipient phone number must be not empty');
+        }
+
+        $data = [
+            'recipient' => $recipient,
+            'text' => $text,
+            'from' => $alphaName
+        ];
+
+        $result = $this->callMethod('Message', 'SendSMSMessage', $data);
+
+        if (!empty($result)) {
+            return $result['data']['messageId'];
+        }
+        else throw new Exception();
     }
 }
